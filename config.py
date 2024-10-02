0d36510984ffdce23ffdd0c311515f271fbd615a
Kennyggkgg <darkphoenixlord101@gmail.com>
2024-10-02 02:16:38 +0100

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-API_KEYS = {
-    'weather': 'your_weather_api_key',
-    'news': 'your_news_api_key',
-    'mailgun': 'your_mailgun_api_key',
-    'mailgun_domain': 'your_mailgun_domain',
-    'smarthome': 'your_smart_home_api_key',
-    'stock': 'your_stock_api_key'
-}
